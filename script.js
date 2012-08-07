41cb79572f7770e7d1a6fd10fb4392d39d24fa78
Calvin Huang <calvin@resilien7.com>
2012-08-07 07:27:19 -0700

    add ToC

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 /**
  * Wikimedia Coding Exercise Script.js  
  */
-/*globals console */
+/*globals console, Node */
 
 $(function(){
 	'use strict';
+	
+	$(".section").hide();
+	
+	// build fixed ToC
+	$('h2').each(function(){
+		var id = this.id,
+				navList = $('#TopNav>ul'),
+				text = $(this).contents()
+					.filter(function() {
+						return this.nodeType === Node.TEXT_NODE;
+					}).text();
+		if (id) {
+				$('<li><a href="#'+id+'" class="toc-link">' + text + '</a></li>').appendTo(navList);
+		}
+	});
+	// expand section when navigated to
+	$('a.toc-link').click(function(){
+		var id = $(this).attr('href'),
+				heading = $(id),
+				section = heading.next('.section');
+		if (section.css('display') === 'none') {
+			section.prev('h2').children('button').click();
+		}
+	});
+	
+	// highlight active nav item
+	$(window).bind('hashchange', function(){
+		var fragment = location.hash;
+		$('#TopNav>ul>li').removeClass('active');
+		$('#TopNav>ul>li>a[href$="'+fragment+'"]').parent().addClass('active');
+	});
+	$('#TopNav>ul>li>a').click(function(){
+		$(window).trigger('hashchange');
+	});
+	
 	// toggle section expand/collapse
 	$('.toggle>button').click(function(event){
 		var section = $(this).parent().next('.section'),
@@ -13,17 +48,11 @@ $(function(){
 		section.toggle();
 		buttonText = section.css('display') === 'none'? 'show' : 'hide';
 		$(this).text(buttonText);
+
 	});
 	$('.toggle').click(function(){
 		$('button', this).click();
 	});
 	
-	// highlight active nav item
-	$(window).bind('hashchange', function() {
-		var fragment = location.hash;
-		console.log(fragment);
-		$('#TopNav>ul>li>a').removeClass('active');
-		$('#TopNav>ul>li>a[href$="'+fragment+'"]').addClass('active');
-	});
-
+	$(window).trigger('hashchange');
 });
