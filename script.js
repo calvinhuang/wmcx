a1ba55b171bbdd76e92913b6d70c7a3cab16bf2b
Calvin Huang <calvin@resilien7.com>
2012-08-07 05:51:04 -0700

    highlight selected nav item

--- script.js
+++ script.js
@@ -5,16 +5,25 @@
 
 $(function(){
 	'use strict';
+	// toggle section expand/collapse
 	$('.toggle>button').click(function(event){
 		var section = $(this).parent().next('.section'),
 				buttonText;
 		event.stopPropagation(); // don't need an infinite loop
 		section.toggle();
-		buttonText = section.css('display') == 'none'? 'show' : 'hide';
+		buttonText = section.css('display') === 'none'? 'show' : 'hide';
 		$(this).text(buttonText);
 	});
-	
 	$('.toggle').click(function(){
 		$('button', this).click();
 	});
+	
+	// highlight active nav item
+	$(window).bind('hashchange', function() {
+		var fragment = location.hash;
+		console.log(fragment);
+		$('#TopNav>ul>li>a').removeClass('active');
+		$('#TopNav>ul>li>a[href$="'+fragment+'"]').addClass('active');
+	});
+
 });
